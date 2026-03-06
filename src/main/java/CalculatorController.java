42c9414f073641ce132de0c10eb2c668e6ca5012
Puntawat Subhamani <puntawatsub@gmail.com>
2026-03-06 05:25:08 +0200

    init

---
+++ src/main/java/CalculatorController.java
@@ -0,0 +1,32 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CalculatorController {
+
+    @FXML private TextField number1Field;
+    @FXML private TextField number2Field;
+    @FXML private Label resultLabel;
+
+    @FXML
+    private void onCalculateClick() {
+        try {
+            double num1 = Double.parseDouble(number1Field.getText());
+            double num2 = Double.parseDouble(number2Field.getText());
+
+            double sum = num1 + num2;
+            double subtract = num1 - num2;
+            double product = num1 * num2;
+            double division = num1 / num2;
+
+            resultLabel.setText("Sum: " + sum + "Difference: " + subtract + ", Product: " + product + ", Quotient: " + division);
+
+            // Save to DB
+            ResultService.saveResult(num1, num2, sum, subtract, product, division);
+
+        } catch (NumberFormatException e) {
+            resultLabel.setText("Please enter valid numbers!");
+        }
+    }
+}
